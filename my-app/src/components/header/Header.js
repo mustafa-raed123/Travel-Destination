55494ab7f327eacc968a32e62ee28f5e0b281eab
mustafa-raed123 <mwsym1898@gmail.com>
2024-05-01 08:25:04 +0000

    add lab16

---
+++ my-app/src/components/header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='container bg-primary text-white text-center mb-5 p-3'>
+      <h1>travel destinations</h1>
+    </div>
+  )
+}
+
+export default Header
